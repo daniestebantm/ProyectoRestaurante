f495f13db128ffbb03ea32c835d028519b098644
sergio <sergio.ilescas@cetys.edu.mx>
2026-05-21 13:46:15 -0700

    si

---
+++ PlatoFuerte.java
@@ -0,0 +1,22 @@
+public class PlatoFuerte extends Platillo {
+    private String tipoProteina;
+    public PlatoFuerte(String nombre, String descripcion, double precio, int tiempoPreparacion,
+                       String tipoProteina) {
+        super(nombre, descripcion, precio, tiempoPreparacion
+        );
+        this.tipoProteina = tipoProteina;
+    }
+    public String getTipoProteina() {
+        return tipoProteina;
+    }
+    @Override
+    public String getTipo() {
+        return "Plato fuerte";
+    }
+    @Override
+    public String toString() {
+
+        return super.toString() +
+                " Proteina: " + tipoProteina;
+    }
+}
---
+++ Postre.java
@@ -0,0 +1,20 @@
+public class Postre extends Platillo {
+    private boolean vegano;
+    public Postre(String nombre, String descripcion, double precio, int tiempoPreparacion
+            , boolean vegano) {
+        super(nombre, descripcion, precio, tiempoPreparacion);
+        this.vegano = vegano;
+    }
+    public boolean isVegano() {
+        return vegano;
+    }
+    @Override
+    public String getTipo() {
+        return "Postre";
+    }
+    @Override
+    public String toString() {
+        return super.toString() +
+                " Vegano: " + vegano;
+    }
+}
